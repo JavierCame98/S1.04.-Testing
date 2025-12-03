041db7892e8944e973957b7b296533a7de9510cd
Javier Castillo <javiercame98@gmail.com>
2025-12-03 11:03:05 +0100

    feat: adding overriding equals and hasCode methods to avoid duplicate books

--- src/S0411/Book.java
+++ src/S0411/Book.java
@@ -12,4 +12,17 @@ public class Book {
         return title;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return title.equals(book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode();
+    }
+
 }
