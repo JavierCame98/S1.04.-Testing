d65898842ba059ff68ca14dc6507231b43350c4e
Javier Castillo <javiercame98@gmail.com>
2025-12-03 07:25:18 +0100

    initial commit feat: creating Book and Library class with its methods

---
+++ src/S0411/Book.java
@@ -0,0 +1,15 @@
+package S0411;
+
+public class Book {
+
+    private String title;
+
+    public Book (String title){
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
---
+++ src/S0411/Library.java
@@ -0,0 +1,51 @@
+package S0411;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private List<Book> booksList;
+
+    public Library (){
+        booksList = new ArrayList<>();
+    }
+
+    public void addBook(Book book){
+        booksList.add(book);
+    }
+
+    public List<Book> getBooksList (){
+        return new ArrayList<>(booksList);
+    }
+
+    public String getBookByIndex(int index){
+        if(index < 0 || index >= booksList.size()){
+            throw new IllegalArgumentException("");
+        }
+        Book book = booksList.get(index);
+        return book.getTitle();
+    }
+
+    public void addBookByIndex(Book b, int index){
+        if(index < 0 || index >= booksList.size()){
+                throw new IllegalArgumentException("");
+        }
+        booksList.add(index, b);
+    }
+
+    public boolean removeBookByTitle(String title) {
+        if(title == null || title.isEmpty()){
+            throw new IllegalArgumentException("");
+        }
+        for (int i = 0; i <= booksList.size(); i++) {
+            Book book = booksList.get(i);
+            if (book.getTitle().equals(title)) {
+                booksList.remove(i);
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
