10840b45e1d82bfec4e7877b7ccd9feb1adea834
Javier Castillo <javiercame98@gmail.com>
2025-12-04 09:58:40 +0100

    initial commit Feat: creating DNICalculation class with letterCalculation method

--- src/S0412/DNICalculation.java
+++ src/S0412/DNICalculation.java
@@ -2,4 +2,16 @@ package S0412;
 
 public class DNICalculation {
 
+    private static final String LETTERS = "RWAGMYFPDXBNJZSQVHLCKE";
+
+    public static char letterCalculation(int DNINumber){
+
+        if(DNINumber <= 0 || DNINumber > 99999999){
+            throw new IllegalArgumentException("The DNI number is out of range");
+        }
+        int indexDNI = DNINumber %23;
+        return LETTERS.charAt(indexDNI);
+    }
+
+
 }
